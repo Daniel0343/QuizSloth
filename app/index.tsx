6165da82fd2ee9888eb6934bb155f11cfb1639ad
Daniel0343 <2006dpm@gmail.com>
2026-04-02 11:46:41 +0200

    Inicio proyecto - Ficheros configuracion

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "./global.css"
+
+const index = () => {
+  return (
+    <View className="p-5">
+      <Text className=' text-red-900'>App</Text>
+    </View>
+  )
+}
+
+export default index
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [ "./App.tsx", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./presentation/**/*.{js,jsx,ts,tsx}"  ],
+}
